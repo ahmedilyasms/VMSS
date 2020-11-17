b89b2205b88bdce5d928fd33d778ebe373fad693
Ahmed Ilyas <61338317+ahmedilyasms@users.noreply.github.com>
2020-11-17 01:41:00 -0800

    Update imagewarmuperror.ps1

--- imagewarmuperror.ps1
+++ imagewarmuperror.ps1
@@ -143,7 +143,7 @@ function IsCosmosDbEmulatorRunning([string] $source)
        $Arguments = "/NoExplorer","/NoTelemetry","/DisableRateLimiting","/NoFirewall","/PartitionCount=25","/NoUI","/DataPath=$dataPath"
 
        Log -dataToLog "Starting Cosmos DB Emulator..."
-       #Start-Process -FilePath $Source -ArgumentList $Arguments -Wait
+       Start-Process -FilePath $Source -ArgumentList $Arguments -Wait
        # This is expected to take < 300 seconds.
        $timeoutSeconds = 300
        Log -dataToLog "Waiting for Cosmos DB Emulator Come to running state within $timeoutSeconds seconds"
